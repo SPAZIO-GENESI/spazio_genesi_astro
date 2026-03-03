dda9273637d3c6d2cc4b36567be790123d3e5cee
maxAlone <mascam64@gmail.com>
2026-03-03 22:18:20 +0100

    Refactor asset paths in configuration and HTML files for consistency across the application

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,12 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+const isProd = process.env.NODE_ENV === 'production';
+
 // https://astro.build/config
 export default defineConfig({ 
-    site: 'https://spazio-genesi.github.io/spazio_genesi_astro/', 
-    base: '/spazio_genesi_astro/',
+    site: isProd
+    ? 'https://spaziogenesi.org'
+    : 'https://spazio-genesi.github.io',
+    base: isProd ? '/' : '/spazio_genesi_astro/',
 });
