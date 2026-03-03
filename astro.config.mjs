8198022640a81b49ea9d782472afe098d007c65b
maxAlone <mascam64@gmail.com>
2026-03-03 20:41:00 +0100

    pubblica astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({ 
+    site: 'https://spazio-genesi.github.io/spazio_genesi_astro/', 
+    base: '/spazio_genesi_astro/',
+});
